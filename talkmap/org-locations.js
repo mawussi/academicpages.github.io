39b8556a2bd0a52106c1215fe281375de5a829c2
Mawussi <mawussi.zounon@gmail.com>
2018-01-22 16:00:36 +0000

    add map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,47 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
+    "Lugano, Switzerland", 
+    46.0050102, 
+    8.9520281
+  ], 
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
+    "Toulouse, France", 
+    43.6044622, 
+    1.4442469
+  ], 
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
+    "Valencia, Spain", 
+    39.4699014, 
+    -0.3759513
+  ], 
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Santiago de Compostela, Spain", 
+    42.8804219, 
+    -8.5458608
+  ], 
+  [
+    "Atlanta Georgia, USA", 
+    33.7490987, 
+    -84.3901849
+  ], 
+  [
+    "Zurich, Switzerland", 
+    47.3723957, 
+    8.5423216
+  ], 
+  [
+    "Denver, Colorado, USA", 
+    39.7391536, 
+    -104.9847034
+  ], 
+  [
+    "Knoxville, Tennessee, USA", 
+    35.9603948, 
+    -83.9210261
+  ], 
+  [
+    "Bengaluru, India", 
+    12.9791198, 
+    77.5912997
   ]
 ];
